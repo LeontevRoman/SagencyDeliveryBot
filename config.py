59bc8299d42675d263da95f27cb44b5d992efbaf
Roman Leontev <97741943+LeontevRoman@users.noreply.github.com>
2023-03-30 22:51:14 +0300

    add virable for webhook

--- config.py
+++ config.py
@@ -5,7 +5,15 @@ from db.db import DataBase
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN_SAGENCY_DELIVERY_BOT')
+DOMAIN = os.getenv('DOMAIN')
+
+WEBHOOK_DOMAIN = f'https://{DOMAIN}'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_DOMAIN}{WEBHOOK_PATH}'
+
+APP_HOST = '0.0.0.0'
+APP_PORT = os.getenv('PORT', default=9000)
 
 db = DataBase()
 
-GROUP_NAME = "@test_test_groupt"
+GROUP_NAME = "@test_test_a"
