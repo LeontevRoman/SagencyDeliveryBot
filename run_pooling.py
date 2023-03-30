5fbeb868425fc1f024880d60ae376d83ba425510
Roman Leontev <97741943+LeontevRoman@users.noreply.github.com>
2023-03-30 23:05:30 +0300

    change setup router

--- run_pooling.py
+++ run_pooling.py
@@ -3,11 +3,7 @@ import logging
 
 from aiogram import Dispatcher, Bot
 from config import TOKEN
-
-from handlers import keyboard_function
-from handlers import join_in_group
-from handlers import commands_bot
-from handlers import block_message_not_member
+from handlers import setup_routers
 
 
 async def run_bot():
@@ -15,13 +11,10 @@ async def run_bot():
     bot = Bot(token=TOKEN, parse_mode="HTML")
     dp = Dispatcher()
 
+    router = setup_routers()
+    dp.include_router(router)
+
     print('Бот вышел в сеть...')
-
-    dp.include_router(block_message_not_member.router)
-    dp.include_router(keyboard_function.router)
-    dp.include_router(join_in_group.router)
-    dp.include_router(commands_bot.router)
-
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
