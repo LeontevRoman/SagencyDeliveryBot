3913cf1bdec596c8b338451da4525e43462d8f31
Roman Leontev <97741943+LeontevRoman@users.noreply.github.com>
2023-03-30 23:37:49 +0300

    add virable for webhook

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ WEBHOOK_PATH = f'/webhook/{TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_DOMAIN}{WEBHOOK_PATH}'
 
 APP_HOST = '0.0.0.0'
-APP_PORT = os.getenv('PORT', default=9000)
+APP_PORT = 443
 
 db = DataBase()
 
